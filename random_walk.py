ec3970e60f9d7ccbfeaa0682c32d514c260e4427
Angela <angelakh.seo@gmail.com>
2022-05-01 19:04:12 -0700

    random colors

--- random_walk.py
+++ random_walk.py
@@ -1,13 +1,23 @@
 from turtle import Turtle, Screen
+import turtle as t
 import random
+
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0,255)
+    b = random.randint(0,255)
+    g = random.randint(0,255)
+    return (r, b, g)
+
 
 tim = Turtle()
 tim.pensize(10)
 tim.speed(50)
-colors = ["red", "pink", "light blue", "orange", "violet", "purple", "green", "aqua"]
 degrees = [0, 90, 180, 270]
 for _ in range(300):
-    tim.color(random.choice(colors))
+    tim.pencolor(random_color())
     tim.forward(25)
     tim.left(random.choice(degrees))
 
